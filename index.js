69175ea1d50869d06bda46f3b7615b61e00518bc
unknown <jemibol106@gmail.com>
2024-05-24 13:51:18 +0100

    update index.js

--- index.js
+++ index.js
@@ -110,6 +110,6 @@ function isValidEmail(email) {
 const server = createServer(handler)
 
 // listen to the server
-server.listen("zuri-node-form-validation.vercel.app", ()=>{
+server.listen(5000, ()=>{
     console.log("listening on localhost")
 })
